75fdd815fe3f903d1214389c9a38724cec55ffca
Ignatius22 <igee54077@gmail.com>
2020-10-12 16:52:28 +0100

    add button component and pass the button name as prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
+import '../App.css'
 import Display from '../components/Display';
-
-
-
+import Button from '../components/Button'
 
 class App extends Component{
 
 
   render(){
     return(
-      <div>
+      <div className='App'>
        <h3>Class components with calc</h3>
       <Display calculation='hello'/>
+      <Button  buttonName = 'click'/>
       </div>
 
     )
