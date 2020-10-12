75c7d65959b3cf389c880e504be8146400ca4421
Ignatius22 <igee54077@gmail.com>
2020-10-12 16:37:28 +0100

    add calculation props with default props

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Display from '../components/Display';
+
+
+
+
+class App extends Component{
+
+
+  render(){
+    return(
+      <div>
+       <h3>Class components with calc</h3>
+      <Display calculation='helloooo'/>
+      </div>
+
+    )
+  }
+}
+
+
+
+export default App;
