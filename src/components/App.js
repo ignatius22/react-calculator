e3be8d2bc801fe7534b06cda712c96358e3ed0b6
Ignatius22 <igee54077@gmail.com>
2020-10-12 15:11:03 +0100

    fix failing checks

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
 
+const App = () => (
 
-const App = () =>{
-  return(
-
-    <h1>hello calc</h1>
-  )
-}
+  <h1>hello calc</h1>
+);
 
 export default App;
