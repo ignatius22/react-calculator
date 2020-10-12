1daadae1e11ceb2998ac16a980610af92d031606
Ignatius22 <igee54077@gmail.com>
2020-10-12 17:09:10 +0100

    add all button groups

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import '../App.css'
+import '../App.css';
 import Display from '../components/Display';
-import Button from '../components/Button'
+import Button from '../components/Button';
+import ButtonPanel from '../components/ButtonPanel'
 
 class App extends Component{
 
@@ -9,9 +10,10 @@ class App extends Component{
   render(){
     return(
       <div className='App'>
-       <h3>Class components with calc</h3>
-      <Display calculation='hello'/>
-      <Button  buttonName = 'click'/>
+       <h3>Calculator App</h3>
+        <Display calculation='hello'/>
+        <Button  buttonName = 'click'/>
+        <ButtonPanel/>
       </div>
 
     )
