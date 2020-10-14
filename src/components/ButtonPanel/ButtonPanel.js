b1c3108b9fb5a037bab0fde0d96fbfc8e72a52bf
Ignatius22 <igee54077@gmail.com>
2020-10-14 07:09:38 +0100

    apply change to implement DRY rule

--- src/components/ButtonPanel/ButtonPanel.js
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import Button from '../Button/Button';
 import './ButtonPanel.css';
-
-
-const buttonArray = [
-    {name: 'AC'},
-    {name: '+/-'},
-    {name: '%'},
-    {name: '÷'},
-    {name: '7'},
-    {name: '8'},
-    {name: '9'},
-    {name: 'X'},
-    {name: '4'},
-    {name: '5'},
-    {name: '6'},
-    {name: '-'},
-    {name: '1'},
-    {name: '2'},
-    {name: '3'},
-    {name: '+'},
-    {name: '0'},
-    {name: '.'},
-    {name: '='},
-
-]
-
-
+import buttonGroup from '../ButtonGroup/ButtonGroup'
 
 
 const ButtonPanel = () => (
   <>
     <div className="panel">
       <div className="group">
-
-      {buttonArray.map((button,i) => (button.name === '0') ?
+      {buttonGroup.map((button,i) => (button.name === '0') ?
       <Button key={i} name={button.name} wide color='#dfdfdf' />:
       (button.name === '+' || button.name === '-' ||
        button.name === '=' || button.name === 'X' ||
